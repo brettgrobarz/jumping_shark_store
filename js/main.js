b7c783260d5d37e32b1a50da0d6da3fc15c93b10
brettgrobarz <brettgrobarz@Bretts-MacBook-Pro.local>
2014-07-27 13:07:52 -0500

    sunday

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
 
   // get the items out of the json file
-  	$.getJSON('items.json', function(json){
-  		console.log(json);
-  	});
+  	// $.getJSON('items.json', function(json){
+  	// 	console.log(json);
+  	// });
 
-  	console.log("hi!!");
+  	// console.log("hi!!");
 
     // add the items from the json file to the DOM using the template in index.html
 
@@ -18,5 +18,40 @@ $(document).ready(function(){
   // attach an event listener to all `.buttons` in the `.cart-item`s to detect clicks
     // when clicked, remove the item from the table in `#cart`
     // run the function you created to update the total
+
+    $(document).ready(function(){
+
+  $.getJSON( "../items.json", function(json) {
+
+    for(var i in json) {
+      $(".items").append("<div class=\"list-item\"><div class=\"list-item-image\"><img src=\"../img/" + json[i].image + "\"></div><div class=\"list-item-header\"><span class=\"list-item-name\">" + json[i].name + "</span><span class=\"list-item-price\">" + json[i].price + "</span></div><div class=\"list-item-add\"><a href=\"#\" class=\"button\">Add to cart</a></div></div>");
+    }
+
+  });
+
+  updateTotal = function() {
+        var total = 0;
+        $(".cart-item-price").each(function() {
+          total += parseInt($(this).html().substring(1));
+        });
+        $("#cart-total").html("$" + total);
+  };
+
+  $(document).on("click", ".list-item-add .button", function (e) {
+        e.preventDefault();
+        var $parent = $(this).parents(".list-item");
+        var name = $parent.find(".list-item-name").html();
+        var price = $parent.find(".list-item-price").html();
+        $("#cart tbody").append("<tr class=\"cart-item\"><td class=\"cart-item-name\">" + name + "</td><td class=\"cart-item-price\">" + price + "</td><td class=\"cart-item-remove\"><a href=\"#\" class=\"button\">Remove</a></td></tr>");
+        updateTotal();
+  });
+
+  $(document).on("click", ".cart-item-remove .button", function (e) {
+        e.preventDefault();
+        $(this).parents(".cart-item").remove();
+        updateTotal();
+  });
+
+});
 
 });
